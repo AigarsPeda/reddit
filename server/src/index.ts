22fe06369804f46711cf3a11537f1032e19eca38
Aigars Peda <aigarspeda@gmail.com>
2020-10-31 09:17:56 +0200

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,34 @@
+import "reflect-metadata";
+import microConfig from "./mikro-orm.config";
+import express from "express";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+
+const PORT = 8000;
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [],
-    dbName: "reddit",
-    type: "postgresql",
-    debug: !__prod__
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false
+    }),
+    context: () => ({ em: orm.em })
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
   });
 };
 
 main();
-
-console.log("Hello World!!");

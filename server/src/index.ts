364b655911b34aae9faa93f9b1cbe0e9567b50ba
Aigars Peda <aigarspeda@gmail.com>
2020-11-02 17:40:19 +0200

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import { MyContext } from "./types";
 import "reflect-metadata";
 import microConfig from "./mikro-orm.config";
 import express from "express";
@@ -8,6 +9,10 @@ import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+// redis
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
 
 const PORT = 8000;
 
@@ -17,12 +22,34 @@ const main = async () => {
 
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: "lax", // csrf
+        secure: __prod__ // cookie only works in https
+      },
+      saveUninitialized: false,
+      secret: "fgdgdfsgg5rgdffgfgdfg",
+      resave: false
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false
     }),
-    context: () => ({ em: orm.em })
+    context: ({ req, res }) => ({ em: orm.em, req, res })
   });
 
   apolloServer.applyMiddleware({ app });

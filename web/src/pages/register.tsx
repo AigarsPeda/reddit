63c32779255d1c0fc59e17d2004b37df58f9931d
Aigars Peda <aigarspeda@gmail.com>
2020-11-03 08:54:06 +0200

    Create register.tsx

---
+++ web/src/pages/register.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type registerProps = {};
+
+const Register: React.FC<registerProps> = () => {
+  return (
+    <div>
+      <h1>Register Page</h1>
+    </div>
+  );
+};
+
+export default Register;

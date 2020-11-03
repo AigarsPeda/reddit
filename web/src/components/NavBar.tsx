c7ce3696500aa4f6e0f81e27b138602cbac6cebc
Aigars Peda <aigarspeda@gmail.com>
2020-11-03 08:53:38 +0200

    Create NavBar.tsx

---
+++ web/src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DarkModeSwitch from "./DarkModeSwitch";
+
+const NavBar: React.FC = () => {
+  return (
+    <nav>
+      <DarkModeSwitch />
+    </nav>
+  );
+};
+
+export default NavBar;

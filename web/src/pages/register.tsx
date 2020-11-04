dee36bc2c9135c70a24b610f8569ed28152f03da
Aigars Peda <aigarspeda@gmail.com>
2020-11-04 06:59:33 +0200

    Update register.tsx

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,12 +1,62 @@
 import React from "react";
+import { Box, Button } from "@chakra-ui/core";
+import { Form, Formik } from "formik";
+
+// utils
+import { toErrorMap } from "../utils/toErrorsMap";
+
+// components
+import Wrapper from "../components/Wrapper";
+import InputField from "../components/InputField";
+import { useRegisterMutation } from "../generated/graphql";
 
 type registerProps = {};
 
 const Register: React.FC<registerProps> = () => {
+  const [, register] = useRegisterMutation();
   return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (value, { setErrors }) => {
+          const response = await register({
+            username: value.username,
+            password: value.password
+          });
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+              autoComplete="on"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+                autoComplete="off"
+              />
+            </Box>
+            <Button
+              type="submit"
+              variantColor="teal"
+              mt={4}
+              isLoading={isSubmitting}
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
   );
 };
 

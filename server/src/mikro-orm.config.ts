8401d99e57c9af51e8722641239b87ee890f3a37
Aigars Peda <aigarspeda@gmail.com>
2020-11-07 06:38:24 +0200

    Update mikro-orm.config.ts

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -1,5 +1,5 @@
-import { User } from "./../entities/User";
-import { Post } from "../entities/Post";
+import { User } from "./entities/User";
+import { Post } from "./entities/Post";
 import { __prod__ } from "./constants";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";

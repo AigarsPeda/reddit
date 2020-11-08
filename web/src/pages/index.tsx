fd80b6db542186552109219a0b285bed5896b863
Aigars Peda <aigarspeda@gmail.com>
2020-11-08 09:34:22 +0200

    Update index.tsx

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { withUrqlClient } from "next-urql";
+import { usePostsQuery } from "../generated/graphql";
 
 const Index: React.FC = () => {
+  const [{ data }] = usePostsQuery();
+
   return (
     <div>
-      <h1>Hello World!</h1>
+      {data ? (
+        data.posts.map((post) => {
+          return <div key={post.id}>{post.title}</div>;
+        })
+      ) : (
+        <div>
+          <h2>Loading...</h2>
+        </div>
+      )}
     </div>
   );
 };
 
-export default Index;
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);

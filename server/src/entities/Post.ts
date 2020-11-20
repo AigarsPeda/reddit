b2b542cf783c1b2260fdb111db93a5ba5d082297
Aigars Peda <aigarspeda@gmail.com>
2020-11-20 10:28:39 +0200

    Update Post.ts

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -4,16 +4,42 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn
 } from "typeorm";
+import { User } from "./User";
 
 @ObjectType()
 @Entity()
 export class Post extends BaseEntity {
+  // field exposes field to GraphQL schema
   @Field()
   @PrimaryGeneratedColumn()
   id!: number;
+
+  @Field()
+  @Column()
+  title!: string;
+
+  @Field()
+  @Column()
+  text!: string;
+
+  @Field()
+  @Column({ type: "int", default: 0 })
+  points!: number;
+
+  // for OneToMany relationship
+  @Field()
+  @Column()
+  creatorId: number;
+
+  // for OneToMany relationship
+  // OneToMany in User.ts and ManyToOne here user.posts is
+  // fields name in User.ts
+  @ManyToOne(() => User, (user) => user.posts)
+  creator: User;
 
   @Field(() => String)
   @CreateDateColumn()
@@ -22,8 +48,4 @@ export class Post extends BaseEntity {
   @Field(() => String)
   @UpdateDateColumn()
   updatedAt: Date;
-
-  @Field()
-  @Column()
-  title!: string;
 }

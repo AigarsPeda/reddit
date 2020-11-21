106d712e563b04a2162dde45aae07127a8279d0d
Aigars Peda <aigarspeda@gmail.com>
2020-11-21 08:21:11 +0200

    Update types.ts

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,10 +1,8 @@
-// import { session } from 'express-session';
-import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
 import { Request, Response } from "express";
 import { Redis } from "ioredis";
 
 export type MyContext = {
-  em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
+  // @ts-ignore
   req: Request & { session: Express.Session };
   redis: Redis;
   res: Response;

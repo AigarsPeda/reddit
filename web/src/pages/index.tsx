549f1c88815fff9207ce06ef364b071061a9b9ee
Aigars Peda <aigarspeda@gmail.com>
2020-11-30 06:37:08 +0200

    Update index.tsx

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,16 +3,28 @@ import { createUrqlClient } from "../utils/createUrqlClient";
 import { withUrqlClient } from "next-urql";
 import { usePostsQuery } from "../generated/graphql";
 import Layout from "../components/Layout";
+import { Box, Heading, Stack, Text } from "@chakra-ui/core";
 
 const Index: React.FC = () => {
-  const [{ data }] = usePostsQuery();
+  const [{ data }] = usePostsQuery({
+    variables: {
+      limit: 10
+    }
+  });
 
   return (
     <Layout>
       {data ? (
-        data.posts.map((post) => {
-          return <div key={post.id}>{post.title}</div>;
-        })
+        <Stack spacing={8}>
+          {data.posts.map((post) => {
+            return (
+              <Box key={post.id} p={5} shadow="md" borderWidth="1px">
+                <Heading fontSize="xl">{post.title}</Heading>
+                <Text mt={4}>{post.textSnippet}</Text>
+              </Box>
+            );
+          })}
+        </Stack>
       ) : (
         <div>
           <h2>Loading...</h2>

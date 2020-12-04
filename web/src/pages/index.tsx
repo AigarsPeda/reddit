0472aa9f0e20340e634714d7810d8c16aa23f507
Aigars Peda <aigarspeda@gmail.com>
2020-12-04 04:50:13 +0200

    Update index.tsx

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -32,6 +32,7 @@ const Index: React.FC = () => {
             return (
               <Box key={post.id} p={5} shadow="md" borderWidth="1px">
                 <Heading fontSize="xl">{post.title}</Heading>
+                <Text>Author: {post.creator.username}</Text>
                 <Text mt={4}>{post.textSnippet}...</Text>
               </Box>
             );

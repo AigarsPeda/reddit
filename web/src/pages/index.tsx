5c74c91faf8b57f101633ddbf51be39d60ccb161
Aigars Peda <aigarspeda@gmail.com>
2020-12-05 13:23:14 +0200

    Update index.tsx

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,7 +3,16 @@ import { createUrqlClient } from "../utils/createUrqlClient";
 import { withUrqlClient } from "next-urql";
 import { usePostsQuery } from "../generated/graphql";
 import Layout from "../components/Layout";
-import { Box, Button, Flex, Heading, Stack, Text } from "@chakra-ui/core";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Icon,
+  IconButton,
+  Stack,
+  Text
+} from "@chakra-ui/core";
 
 const Index: React.FC = () => {
   const [variables, setVariables] = useState({
@@ -30,11 +39,33 @@ const Index: React.FC = () => {
         <Stack spacing={8}>
           {data.posts.posts.map((post) => {
             return (
-              <Box key={post.id} p={5} shadow="md" borderWidth="1px">
-                <Heading fontSize="xl">{post.title}</Heading>
-                <Text>Author: {post.creator.username}</Text>
-                <Text mt={4}>{post.textSnippet}...</Text>
-              </Box>
+              <Flex key={post.id} p={5} shadow="md" borderWidth="1px">
+                <Flex
+                  flexDirection="column"
+                  align="center"
+                  justifyContent="center"
+                  mr={4}
+                >
+                  <IconButton
+                    icon="chevron-up"
+                    size="md"
+                    aria-label="up vote"
+                    background="none"
+                  />
+                  <p>{post.points}</p>
+                  <IconButton
+                    icon="chevron-down"
+                    size="md"
+                    aria-label="down vote"
+                    background="none"
+                  />
+                </Flex>
+                <Box>
+                  <Heading fontSize="xl">{post.title}</Heading>
+                  <Text>Author: {post.creator.username}</Text>
+                  <Text mt={4}>{post.textSnippet}...</Text>
+                </Box>
+              </Flex>
             );
           })}
         </Stack>

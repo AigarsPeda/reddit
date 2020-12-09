5a0245d8c8c94a644c3066d57e92bbfd3bc82567
Aigars Peda <aigarspeda@gmail.com>
2020-12-09 07:28:11 +0200

    Update Post.ts

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -1,21 +1,20 @@
-import { Field, ObjectType } from "type-graphql";
+import { ObjectType, Field, Int } from "type-graphql";
 import {
-  BaseEntity,
-  Column,
-  CreateDateColumn,
   Entity,
-  ManyToOne,
-  OneToMany,
+  Column,
   PrimaryGeneratedColumn,
-  UpdateDateColumn
+  CreateDateColumn,
+  UpdateDateColumn,
+  BaseEntity,
+  ManyToOne,
+  OneToMany
 } from "typeorm";
-import { Updoot } from "./Updoot";
 import { User } from "./User";
+import { Updoot } from "./Updoot";
 
 @ObjectType()
 @Entity()
 export class Post extends BaseEntity {
-  // field exposes field to GraphQL schema
   @Field()
   @PrimaryGeneratedColumn()
   id!: number;
@@ -32,14 +31,13 @@ export class Post extends BaseEntity {
   @Column({ type: "int", default: 0 })
   points!: number;
 
-  // for OneToMany relationship
+  @Field(() => Int, { nullable: true })
+  voteStatus: number | null; // 1 or -1 or null
+
   @Field()
   @Column()
   creatorId: number;
 
-  // for OneToMany relationship
-  // OneToMany in User.ts and ManyToOne here user.posts is
-  // fields name in User.ts
   @Field()
   @ManyToOne(() => User, (user) => user.posts)
   creator: User;
